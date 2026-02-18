7d2ddcc85fa011548a674633ccd355e7fcfff3f3
Matheus Vitor Sousa Almeida <matheus.almeida@trf1.jus.br>
2026-02-18 18:41:45 -0300

    retirada do prisma e instalação do drizzle

--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,6 @@
-import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client";
-import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import 'dotenv/config';
+import { drizzle } from "drizzle-orm/mysql2";
 
-const connectionString = process.env.DATABASE_URL!;
-const adapter = new PrismaMariaDb({
-	host: process.env.DATABASE_HOST,
-	user: process.env.DATABASE_USER,
-	password: process.env.DATABASE_PASSWORD,
-	database: process.env.DATABASE_NAME,
-	connectionLimit: 5,
-});
-
-const database = new PrismaClient({
-	adapter,
-	log: ["info", "warn", "error"],
-	errorFormat: "pretty",
-});
+const database = drizzle(process.env.DATABASE_URL!);
 
 export { database };
---
+++ src/tables/nota-fiscal-table.ts
@@ -0,0 +1,10 @@
+import { boolean, date, mysqlTable, varchar } from "drizzle-orm/mysql-core";
+
+export const notaFiscalTable = mysqlTable('notafiscal', {
+	id: varchar({length: 255}).primaryKey(),
+	description: varchar({length: 999}).notNull().default(''),
+	link: varchar({length: 999}).notNull(),
+	data: date().notNull(),
+	check: boolean().default(false)
+});
+
